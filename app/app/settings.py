223f2b480b41c77ae33880405a90c1fa79a1997a
Toxelor <alekz69420@gmail.com>
2023-12-15 22:21:47 +0300

    secret

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+secret = os.getenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3r^$qdy+%kaf7$e&m7h+ju^!$st(u%48so8#rexaax-0t@1=*^'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
